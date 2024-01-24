832041d5cec251d9b6a93a3b0ed9c73393935c7f
GabrielBackCode <luga1606@outlook.com>
2024-01-24 11:14:30 -0200

    meu primeiro commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let nome = "Felipe"
+let numeroDeXP = 1000
+
+if (numeroDeXP <= 1000) {
+     console.log(" O Heroi de nome " + nome + " esta no nivel Ferro.")
+
+}else if (numeroDeXP <= 2000){
+	console.log(" O Heroi de nome " + nome + " esta no nivel Bronze.")
+    
+}else if (numeroDeXP <= 5000){
+    console.log(" O Heroi de nome " + nome + " esta no nivel Prata.")
+
+}else if (numeroDeXP <= 7000){
+    console.log(" O Heroi de nome " + nome + " esta no nivel Ouro.")
+    
+}else if (numeroDeXP <= 8000){
+    console.log(" O Heroi de nome " + nome + " esta no nivel Platina.")
+    
+}else if (numeroDeXP <= 9000){
+    console.log(" O Heroi de nome " + nome + " esta no nivel Ascendente.")
+    
+}else if (numeroDeXP <= 10000){
+    console.log(" O Heroi de nome " + nome + " esta no nivel Imortal.")
+    
+}else {
+    console.log(" O Heroi de nome " + nome + " esta no nivel Radiante.")
+}
+    
+    
